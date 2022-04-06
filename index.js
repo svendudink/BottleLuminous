21b4e5050035f93d16fefc1425cbcc2783c61f24
Sven Dudink <sven@dudink.net>
2022-04-06 22:54:42 +0200

    this commit

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ setTimeout(() => {
       await git.add(".");
       await git.commit(commitMessage, { "--date": DATE });
       await git.push("origin", branch);
-      await git;
+      await git.checkoutBranch();
     } else {
       console.log("push is off");
     }
