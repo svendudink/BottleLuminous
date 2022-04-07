526e124ac31e73d7ed2004f75755f0bccddc72ba
Sven Dudink <sven@dudink.net>
2022-04-07 11:28:52 +0200

    next commit

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ import { simpleGit, CleanOptions } from "simple-git";
 
 simpleGit().clean(CleanOptions.FORCE);
 
+const options = {
+  baseDir: process.cwd(),
+  binary: "git",
+  maxConcurrentProcesses: 6,
+  trimmed: false,
+};
+
 simpleGit().add(".").commit("next commit").push("origin", "main");
