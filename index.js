7c4db638b5e4e3c81bbe669628848f76dedfa30f
Sven Dudink <sven@dudink.net>
2023-02-07 13:01:03 +0100

    new file commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { simpleGit, CleanOptions } from "simple-git";
+import dateFormat, { masks } from "dateformat";
+import fs from "fs";
 
-simpleGit().clean(CleanOptions.FORCE);
+let newData = "";
 
 const options = {
   baseDir: process.cwd(),
@@ -10,12 +12,38 @@ const options = {
 };
 const git = simpleGit(options);
 
-`Merge branch 'main' of https://github.com/svendudink/BottleLuminous
+const interval = 28800000;
+//get random number
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+const readWrite = async () => {
+  fs.readFile("date.txt", "utf-8", (err, data) => {
+    if (err) throw err;
+    newData = Math.round(Number(data) + getRandomArbitrary(0, interval));
+    console.log("checksum", newData);
+    fs.writeFile("date.txt", newData.toString(), (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+      console.log(dateFormat(newData, "ddd mmm d H:MM:ss yyyy"));
+    });
+  });
+
+  console.log("testnew");
+};
+
+readWrite();
+
+console.log(dateFormat(1649330110000, "ddd mmm d h:MM:ss yyyy"));
+const commitMessage = "this commit";
+
+const commmitDate = `${commitMessage}
 
 # Please enter the commit message for your changes. Lines starting
 # with '#' will be ignored, and an empty message aborts the commit.
 #
-# Date:      Wed May 18 12:19:33 2022 +0200
+# Date:      ${newData} +0200
 #
 # On branch main
 # Your branch is up to date with 'origin/main'.
@@ -25,8 +53,10 @@ const git = simpleGit(options);
 const gitAdder = async () => {
   console.log("test");
   await git.add(".");
-  await git.commit("testingGitcommit");
+  await git.commit("new file commit");
   await git.push("origin", "main");
 };
 
 gitAdder();
+
+setTimeout(() => {}, 2000);
